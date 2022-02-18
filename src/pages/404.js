3902b67f3b522e5c12fe5a53026b090de30444f1
김정윤 <kjyjh9930@gmail.com>
2022-02-18 13:57:05 +0900

    modify : 404 page title 적용

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Seo } from '../components/seo'
 import { Layout } from '../components/layout'
 
 const NotFound = () => (
   <Layout belongs="none">
+    <Seo title="404 Not Found" />
     <h1>404 Not Found</h1>
-    <strong>This request URL is not found on this server</strong>
+    <div>This request URL is not found on this server</div>
   </Layout>
 )
 
